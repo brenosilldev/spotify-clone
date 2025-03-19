dc1d43899bcf40e4d5099192139976838a4d11d4
breno-spartacode <109807534+brenosilldev@users.noreply.github.com>
2025-03-19 00:00:14 -0300

    :wastebasket: remove:  Configuraçaõ de swagger

--- back/src/server.js
+++ back/src/server.js
@@ -1,55 +1,29 @@
 import  express  from "express";
 import dotenv from "dotenv";
 import cors from 'cors'
-// import { dbConnection} from "./lib/db.js"
-import swaggerJsdoc  from 'swagger-jsdoc'
-import swaggerUi  from 'swagger-ui-express';
-import routerAdmin from "./api/routes/admin.router.js";
-import routerUser from "./api/routes/user.route.js";
+
+import RouterAdmin from "./api/routes/admin.router.js";
+import RouterUser from "./api/routes/user.route.js";
+import RouterSong from "./api/routes/song.route.js";
+import RouterAlbum from "./api/routes/album.route.js";
+import RouterState from "./api/routes/stat.route.js";
+import RouterAuth from "./api/routes/auth.router.js";
+
+
 import { connectDB } from "./config/db.js";
-
-import path from "path";
-import { fileURLToPath } from "url";
-
-// Criar `__dirname` manualmente
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
 
 
 const app = express();
 const port = process.env.PORT || 5000
 dotenv.config();
 
-const swaggerOptions = {
-    definition: {
-      openapi: '3.0.0',
-      info: {
-        title: 'API de Música',
-        version: '1.0.0',
-        description: 'API para gerenciamento de usuários, músicas e álbuns',
-        contact: {
-          name: 'Desenvolvedor',
-          email: 'dev@exemplo.com'
-        }
-      },
-      servers: [
-        {
-          url: `http://localhost:${port}`,
-          description: 'Servidor de desenvolvimento'
-        }
-      ]
-    },
-    // Caminho para os arquivos que contêm anotações JSDoc
-    apis: [path.join(__dirname, "./routes/*.js")], // Caminho absoluto
 
-  };
-
-const specs = swaggerJsdoc(swaggerOptions);
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(specs));
-
-app.use('/v1/users', routerUser)
-app.use('/v1/admin', routerAdmin)
+app.use('/v1/users', RouterUser)
+app.use('/v1/admin', RouterAdmin)
+app.use('/v1/auth', RouterAuth)
+app.use('/v1/songs', RouterSong)
+app.use('/v1/albums', RouterAlbum)
+app.use('/v1/stats', RouterState)
 
 
 app.use(cors({
