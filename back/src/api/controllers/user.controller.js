88be60e518a6ea6681c8e4fa4b665f5109f5e562
breno-spartacode <109807534+brenosilldev@users.noreply.github.com>
2025-03-18 23:57:17 -0300

    ✨ feat: controller de user

---
+++ back/src/api/controllers/user.controller.js
@@ -0,0 +1,42 @@
+
+
+import {User}  from "../models/user.model.js";
+
+const CreateUser = async (req, res) => {
+
+    try{
+        const {id,name ,imageurl,email} = req.body
+
+        const user = await User.find({clerkID:id})
+
+        if(!user){
+            const newUser = new User({
+                clerkID:id,
+                name,
+                email,
+                imageurl
+            })
+
+            const savedUser = await newUser.save()
+            res.status(200).json({success:true, message:"User Created",user:savedUser})
+        }
+
+    }catch(error){
+        res.status(500).json({message:"Internal Server Error",error})
+    }
+
+}
+
+const getAllUsers = async (req, res) => {
+    try {
+        const users = await User.find();
+        res.json(users);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Internal server error' });
+    }
+};
+
+
+
+export{ CreateUser, getAllUsers}
