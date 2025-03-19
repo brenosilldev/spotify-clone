15df3a26b2febe8b18db27e2d26b6c7c79f3c0fd
breno-spartacode <109807534+brenosilldev@users.noreply.github.com>
2025-03-18 23:59:15 -0300

    :truck: chore: Alterado o console.log

--- back/src/config/db.js
+++ back/src/config/db.js
@@ -2,9 +2,10 @@ import mongoose from "mongoose";
 
 export const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.MONGO_URI);
-        console.log("Connected to MongoDB");
+        const conn = await mongoose.connect(process.env.MONGO_URL);
+        console.log(`MongoDB Connected: ${conn.connection.host}`);
     } catch (error) {
         console.log(error);
+        process.exit(1)
     }
 }
