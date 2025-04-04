92dbfb705b48ba40089a481840db8645a9c317cc
Breno Silva <109807534+brenosilldev@users.noreply.github.com>
2025-04-04 14:14:09 -0300

    ✨ feat: Alterado formato do arquivo

--- back/src/server.js
+++ back/src/server.js
@@ -2,6 +2,10 @@ import  express  from "express";
 import dotenv from "dotenv";
 import cors from 'cors'
 import { clerkMiddleware} from "@clerk/express"
+import path from "path";
+import fileUpload from "express-fileupload";
+import { connectDB } from "./config/db.js";
+
 
 import RouterAdmin from "./api/routes/admin.router.js";
 import RouterUser from "./api/routes/user.route.js";
@@ -11,29 +15,41 @@ import RouterState from "./api/routes/stat.route.js";
 import RouterAuth from "./api/routes/auth.router.js";
 
 
-import { connectDB } from "./config/db.js";
 
 
 const app = express();
 const port = process.env.PORT || 5000
 dotenv.config();
 
+const _dirname = path.resolve();
 
+app.use(cors({
+    origin: 'http://localhost:3000',
+    credentials: true,
+}))
+
+app.use(express.json({ limit: '50mb' })); 
+
+
+app.use(clerkMiddleware({  }))
 app.use('/v1/users', RouterUser)
 app.use('/v1/admin', RouterAdmin)
 app.use('/v1/auth', RouterAuth)
 app.use('/v1/songs', RouterSong)
 app.use('/v1/albums', RouterAlbum)
 app.use('/v1/stats', RouterState)
-app.use(clerkMiddleware({  }))
-
-app.use(cors({
-    origin: 'http://localhost:5173',
-    allowedHeaders: "Content-Type,Authorization",
-    credentials: true,
-}))
-
-app.use(express.json({ limit: '50mb' })); 
+app.use(fileUpload({
+    useTempFiles: true,
+    tempFileDir: path.join(_dirname, 'tmp'),
+    createParentPath: true,
+    limits: { fileSize: 10 * 1024 * 1024 }
+}));
+app.use((err, req, res, next) => {
+    console.error("Erro interno:", err);
+    res.status(500).json({
+        message: process.env.NODE_ENV === "production" ? "Internal Server Error" : err.message,
+    });
+});
 
 
 app.listen(port, () => {
