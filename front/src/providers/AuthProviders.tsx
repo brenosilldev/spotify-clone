97e8ad0d59c1bf87cef0553b62546c9116c9c4eb
Breno Silva <109807534+brenosilldev@users.noreply.github.com>
2025-04-04 14:07:09 -0300

    ✨ feat: Enviando o token nas requisiçoes

---
+++ front/src/providers/AuthProviders.tsx
@@ -0,0 +1,59 @@
+import api from "@/lib/Axios";
+import { useAuth } from "@clerk/clerk-react";
+import { Loader } from "lucide-react";
+import {  useEffect, useState } from "react";
+
+
+const updateApiToken = (token:string | null)=>{
+    if(token){
+        api.defaults.headers.common["Authorization"] = `Bearer ${token}`
+    }else{
+        delete api.defaults.headers.common["Authorization"]
+    }
+}
+
+
+
+const AuthProvider = ({children }: { children: React.ReactNode }) => {
+
+    const {getToken} = useAuth();
+    const  [loading,setLoading] = useState(true)
+
+    useEffect(() => {
+        const initAuth = async()=>{
+            try{
+
+                const token = await getToken();
+                updateApiToken(token);
+
+
+            }catch(erro:any){3
+                updateApiToken(null);
+               console.log(erro) 
+            }finally{
+                setLoading(false)
+            }
+        }
+
+        initAuth()
+        
+    },[getToken])
+
+    if(loading){
+         return(
+            <div className="h-screen flex justify-center items-center">
+                <Loader className="size-24 text-green-500 animate-spin"/>
+
+
+            </div>
+       )
+    }
+
+    return (
+        <div>
+            {children}
+        </div>
+    )
+}
+
+export default AuthProvider
