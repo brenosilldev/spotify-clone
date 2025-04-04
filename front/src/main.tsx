90f94ddb336deb8d91df1da98e64a5a838165eee
Breno Silva <109807534+brenosilldev@users.noreply.github.com>
2025-04-04 14:08:16 -0300

    ✨ feat:

--- front/src/main.tsx
+++ front/src/main.tsx
@@ -3,6 +3,9 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import { ClerkProvider } from '@clerk/clerk-react'
+import { BrowserRouter } from 'react-router-dom'
+import AuthProvider from './providers/AuthProviders.tsx'
+
 
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
 
@@ -12,8 +15,13 @@ if (!PUBLISHABLE_KEY) {
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-   <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl="/">
-      <App />
-    </ClerkProvider>
+      <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl="/">
+      <AuthProvider>
+          <BrowserRouter>
+                <App />
+          </BrowserRouter>
+
+      </AuthProvider>
+        </ClerkProvider>
   </StrictMode>,
 )
