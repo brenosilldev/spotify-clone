bad775f869eff81b01f601afaa8f7892383d4d80
Breno Silva <109807534+brenosilldev@users.noreply.github.com>
2025-04-04 14:22:52 -0300

    ✨ feat: Criado arquvio de songs

---
+++ back/src/api/models/song.js
@@ -0,0 +1,49 @@
+import mongoose from "mongoose";
+
+
+const Schema = mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    artist: {
+        type: String,
+        required: true
+    },
+    AudioUrl: {
+        type: String,
+        required: true
+    },
+    imageurl :{
+        type:String
+    },
+    duration: {
+        type: String,
+        required: true
+    },  
+    year: {
+        type: String,
+        required: true
+    },  
+    views: {
+        type: Number,
+        required: false
+    },  
+    likes: {
+        type: Number,
+        required: false
+    },  
+    dislikes: {
+        type: Number,
+        required: false
+    },  
+    album: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Album",
+        required: false     
+    },  
+} , {timestamps: true});
+
+ const Song = mongoose.model("Song", Schema);
+
+ export default Song
