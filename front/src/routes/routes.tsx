971a7cdbc6323d45d4207f766c641d66e5f1e2ed
breno silva <109807534+brenosilldev@users.noreply.github.com>
2025-04-26 13:32:43 -0300

    🚚 chore: alterado caminho do componente

--- front/src/routes/routes.tsx
+++ front/src/routes/routes.tsx
@@ -2,7 +2,7 @@ import { Routes,Route } from "react-router-dom"
 import AuthCallback from "@/pages/auth-callback"
 import Home from "@/pages/home/Home"
 import { AuthenticateWithRedirectCallback } from "@clerk/clerk-react"
-import MainLayout from "@/layout/MainLayout"
+import MainLayout from "@/components/layout/MainLayout"
 import ChatPage from "@/pages/chat/ChatPage"
 import Albums from "@/pages/albums"
 
