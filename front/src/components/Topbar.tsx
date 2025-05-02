eadfe6a7082973644db41ff4725965dd94d73783
Breno Silva <brenosill@hotmail.com>
2025-05-02 00:16:16 -0300

    🚚 chore: Alterado componente

--- front/src/components/Topbar.tsx
+++ front/src/components/Topbar.tsx
@@ -1,36 +1,53 @@
 import { LayoutDashboardIcon } from "lucide-react";
 import { Link } from "react-router-dom";
-import {SignedOut,SignedIn,SignOutButton } from "@clerk/clerk-react";
+import {SignedOut, UserButton } from "@clerk/clerk-react";
 import SingInOAtuhButtons from "./SingInOAtuhButtons";
+import { useUser } from "@/store/useUser";
+import { cn } from "@/lib/utils";
+import { buttonVariants } from "./ui/button";
 
 const Topbar = () => {
-    const isAdmin = false;
-  return (
-    <div>
-        <div className="flex justify-between items-center p-4 sticky top-0 bg-zinc-900/75 backdrop-blur-md z-10">
-            <div className="flex gap-2 itens-center">
+
+    const isAdmin = useUser()
+
+
+    return (
+
+        <div className="flex justify-between items-center px-4 py-4  sticky top-0 bg-zinc-900/75 backdrop-blur-md z-10">
+            <div className="flex gap-2 items-center">
+                <img src="/spotify.png" alt="Spotify" className="w-8 h-8" />
                 Spotify
 
             </div>
-            <div className="flex gap-4 itens-center">
+            <div className="flex gap-4 items-center">
+                
                 {isAdmin && (
-                    <Link to='/admin'>
+                    <Link to='/admin'
+                        className={cn(
+                            buttonVariants({
+                                variant: "outline",})
+                        )}                    >
+                        
+
                         <LayoutDashboardIcon className="size-4 mr-2"/>
-                        Amdin Dashboard
+                        <span>Admin Dashboard</span>
+                    
                     </Link>
                 )}
 
-                <SignedIn>
+                {/* <SignedIn>
                     <SignOutButton />
-                </SignedIn>
+                </SignedIn> */}
+
                 <SignedOut>
                     <SingInOAtuhButtons />
                 </SignedOut>
 
+                <UserButton />
+
             </div>
         </div>
-      
-    </div>
+   
   )
 }
 
