dacaba6bfac663dbfc2ed867cda7bd375c891b6b
Breno Silva <brenosill@hotmail.com>
2025-05-02 00:12:45 -0300

    ✨ feat: Criado estado de gerenciamento de usuário

--- front/src/store/useUser.ts
+++ front/src/store/useUser.ts
@@ -1,8 +1,35 @@
 import { create } from "zustand";
+import api from "@/lib/Axios";
 
+interface UserStore {
+    isAdmin: boolean,
+    isLoading: boolean,
+    error : string | null,
+    checkAdminStatus : () => Promise<void>,
+    reset: () => void,
+}
 
-
-
-export const useUser = create((set) => ({
+export const useUser = create<UserStore>((set) => ({
+    isAdmin: false,
+    isLoading: true,
+    error: null,
+    checkAdminStatus: async () => {
+        set({ isLoading: true, error: null })
+        try {
+            const response = await api.get('/users/check_admin')
+            set({ isAdmin: response.data.admin })
+        } catch (error: any) {
+            set({ error: error.response.data.message , isAdmin: false })
+        } finally {
+            set({ isLoading: false })
+        }
+    },    
+    reset: () => {
+        set({
+            isAdmin: false,
+            isLoading: false,
+            error: null,
+        })
+    }
 
 }));
